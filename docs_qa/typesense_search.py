9983a28b8f4b71663b0f585156f7e624d9cf24d5
Benjamin D. Brodie <digdir@bdbrodie.com>
2023-11-22 19:19:59 +0100

    revert typesense collection name change

--- docs_qa/typesense_search.py
+++ docs_qa/typesense_search.py
@@ -20,7 +20,7 @@ async def typesense_search_multiple(search_queries: GeneratedSearchQueries):
         "searches":
             [
                 {
-                    "collection":"altinn-studio-docs-dev",                
+                    "collection":"altinn-studio-docs",                
                     "q": query,
                     "query_by":"hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,hierarchy.lvl5,hierarchy.lvl6,content,embedding",
                     "include_fields":"hierarchy.lvl0,hierarchy.lvl1,hierarchy.lvl2,hierarchy.lvl3,hierarchy.lvl4,url_without_anchor,type,id,content_markdown",
